fce5194cf7829db14ef42270411599b025a9c668
Vivi <viviane.souza05@aluno.ifce.edu.br>
2023-05-02 11:04:00 -0300

    update

---
+++ project3/bin/project3.dart
@@ -0,0 +1,12 @@
+// tipos de dados BOOLEAN
+
+void main(List<String> arguments) {
+  var varTrue = true;
+  bool varFalse = false; // o falso já é o padrão do boolean
+
+  print(varTrue);
+  print(varFalse);
+  print(!varTrue);
+  print(!varFalse);
+  print(varTrue == varFalse);
+}
