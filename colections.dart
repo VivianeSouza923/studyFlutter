040816a1fda63b126485af82d8a0b914a803fd37
Vivi <70662490+VivianeSouza923@users.noreply.github.com>
2023-05-26 17:42:57 -0300

    Add files via upload

---
+++ colections.dart
@@ -0,0 +1,46 @@
+// coleções
+
+void main() {
+  List nomes = [
+    "Vivi",
+    "Alesi",
+    "Thorin",
+    10,
+    false,
+    5.8,
+  ];
+
+  print(nomes);
+
+  //List<type>
+  List<dynamic> nomes0 = [
+
+  ];
+
+  print(nomes.length);
+  print(nomes.first);
+  print(nomes.last);
+  print(nomes[3]);
+
+
+
+  /*
+  var nome = "Viviane";
+  nome = "Alesi";
+  //nome = 3;
+
+  var idade = 40;
+  idade++;
+
+  dynamic variavel = "Thorin";
+  variavel = 6;
+  variavel = true;
+
+  num numero = 1;
+  numero = 1.1;
+}
+
+void funcao(num numero){
+
+}*/
+}
