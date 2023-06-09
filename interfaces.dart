3989c9c6f073ce693dd8357c9c8770d6ee5678ab
Vivi <viviane.souza05@aluno.ifce.edu.br>
2023-06-09 17:22:53 -0300

    update

--- interfaces.dart
+++ interfaces.dart
@@ -1,8 +1,29 @@
 // interfaces
 
-void main() {}
+void main() {
+  RepositorioPessoas repo = RepositorioPessoas();
+  repo.ler(10);
+  repo.adicionar("Souza");
+  repo.apagar(5);
+}
 
-class RepositorioPessoas {
+abstract class RepositorioPessoas {
+  String ler(int i);
+  void adicionar(String nome);
+  void apagar(int i);
+}
+
+class RepositorioPessoasRemote {
+  String ler(int i) {
+    return "Vivi";
+  }
+
+  void adicionar(String nome) {}
+
+  void apagar(int i) {}
+}
+
+class RepositorioPessoasLocal {
   String ler(int i) {
     return "Vivi";
   }
